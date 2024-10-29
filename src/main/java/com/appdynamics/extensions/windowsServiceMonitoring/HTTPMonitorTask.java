ea03c65ef77b79775a36813b380cf137dd32d608
suyprasa <suyprasa@cisco.com>
2024-10-29 16:43:35 +0530

    Added functionality to monitor services based on regex

--- src/main/java/com/appdynamics/extensions/windowsServiceMonitoring/HTTPMonitorTask.java
+++ src/main/java/com/appdynamics/extensions/windowsServiceMonitoring/HTTPMonitorTask.java
@@ -26,58 +26,73 @@ public class HTTPMonitorTask implements AMonitorTaskRunnable {
     private Map<String, String> server;
     private String metricPrefix;
     private List<Map<String, ?>> metricList;
+    private boolean isRegex;
 
     public HTTPMonitorTask(MonitorContextConfiguration configuration, MetricWriteHelper metricWriteHelper,
-                           Map<String, String> server) {
+                           Map<String, String> server, boolean isRegex) {
         this.configuration = configuration;
         this.metricWriteHelper = metricWriteHelper;
         this.server = server;
+        this.isRegex = isRegex;
         this.metricPrefix = configuration.getMetricPrefix() + DEFAULT_METRIC_SEPARATOR;
         this.metricList = (List<Map<String, ?>>) configuration.getConfigYml().get(METRICS);
         AssertUtils.assertNotNull(this.metricList, "The 'metrics' section in config.yml is either null or empty");
-        logger.info("Initialized HTTPMonitorTask for service: {}", server.get(SERVICE_NAME));
+        logger.info("Initialized HTTPMonitorTask for service: {}", server.get(isRegex ? "serviceRegex" : SERVICE_NAME));
     }
 
     @Override
     public void run() {
-        logger.debug("Starting data collection for service: {}", server.get(SERVICE_NAME));
+        logger.debug("Starting data collection for service: {}", server.get(isRegex ? "serviceRegex" : SERVICE_NAME));
 
         List<Metric> metrics = new ArrayList<>();
 
         try {
             WindowsServiceDataCollector dataCollector = new WindowsServiceDataCollector();
-            Map<String, Long> data = dataCollector.collectData(server.get(SERVICE_NAME));
+            Map<String, Map<String, Object>> allData;
 
-            if (data != null) {
-                logger.debug("Collected data for service {}: {}", server.get(SERVICE_NAME), data);
-
-                Metric metric1 = new Metric(server.get(SERVICE_NAME), String.valueOf(data.get(SERVICE_UP_TIME_IN_SEC)),
-                        metricPrefix + SERVICE_UP_TIME_IN_SEC + DEFAULT_METRIC_SEPARATOR + server.get(SERVICE_NAME));
-                Metric metric2 = new Metric(server.get(SERVICE_NAME), String.valueOf(data.get(SERVICE_STATUS)),
-                        metricPrefix + SERVICE_STATUS + DEFAULT_METRIC_SEPARATOR + server.get(SERVICE_NAME));
-
-                metrics.add(metric1);
-                metrics.add(metric2);
-
-                logger.info("Collected metrics for service {}: [serviceUpTimeInSec: {}, serviceStatus: {}]",
-                        server.get(SERVICE_NAME), data.get(SERVICE_UP_TIME_IN_SEC), data.get(SERVICE_STATUS));
+            if (isRegex) {
+                String serviceNamePattern = server.get("serviceRegex");
+                allData = dataCollector.collectDataUsingRegex(serviceNamePattern);
             } else {
-                logger.warn("No data collected for service: {}", server.get(SERVICE_NAME));
+                String serviceName = server.get(SERVICE_NAME);
+                allData = dataCollector.collectData(serviceName);
+            }
+
+            if (allData != null) {
+                for (Map.Entry<String, Map<String, Object>> entry : allData.entrySet()) {
+                    String serviceName = entry.getKey();
+                    Map<String, Object> data = entry.getValue();
+
+                    logger.debug("Collected data for service {}: {}", serviceName, data);
+
+                    Metric metric1 = new Metric(serviceName, String.valueOf(data.get(SERVICE_UP_TIME_IN_SEC)),
+                            metricPrefix + SERVICE_UP_TIME_IN_SEC + DEFAULT_METRIC_SEPARATOR + serviceName);
+                    Metric metric2 = new Metric(serviceName, String.valueOf(data.get(SERVICE_STATUS)),
+                            metricPrefix + SERVICE_STATUS + DEFAULT_METRIC_SEPARATOR + serviceName);
+
+                    metrics.add(metric1);
+                    metrics.add(metric2);
+
+                    logger.info("Collected metrics for service {}: [serviceUpTimeInSec: {}, serviceStatus: {}]",
+                            serviceName, data.get(SERVICE_UP_TIME_IN_SEC), data.get(SERVICE_STATUS));
+                }
+            } else {
+                logger.warn("No data collected for service: {}", server.get(isRegex ? "serviceRegex" : SERVICE_NAME));
             }
         } catch (Exception e) {
-            logger.error("Error collecting data for service: {}", server.get(SERVICE_NAME), e);
+            logger.error("Error collecting data for service: {}", server.get(isRegex ? "serviceRegex" : SERVICE_NAME), e);
         }
 
         try {
             metricWriteHelper.transformAndPrintMetrics(metrics);
-            logger.info("Metrics successfully reported for service: {}", server.get(SERVICE_NAME));
+            logger.info("Metrics successfully reported for service: {}", server.get(isRegex ? "serviceRegex" : SERVICE_NAME));
         } catch (Exception e) {
-            logger.error("Error reporting metrics for service: {}", server.get(SERVICE_NAME), e);
+            logger.error("Error reporting metrics for service: {}", server.get(isRegex ? "serviceRegex" : SERVICE_NAME), e);
         }
     }
 
     @Override
     public void onTaskComplete() {
-        logger.info("All tasks for service {} finished", this.server.get(SERVICE_NAME));
+        logger.info("All tasks for service {} finished", this.server.get(isRegex ? "serviceRegex" : SERVICE_NAME));
     }
 }
